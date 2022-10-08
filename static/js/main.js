19bcffd989e39b50cbcb094d7534ee581fd310f8
kapilmaurya <kapilmaurya323@gmail.com>
2022-10-08 09:22:01 +0530

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+// function next(){
+//     var first=document.getElementById('first-step')
+//     var second=document.getElementById('second-step')
+//     var next=document.getElementById('bttn')
+//     // console.log(next[0].value)
+//     if(bttn.value=='Next'){
+//         first.style.display="none";
+//         second.style.right=0;
+//     }
+    
+    
+// }
